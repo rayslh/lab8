449db920337ee8695161e6f145b14e2f318518c8
Никита Коломыцев <shishki4l0ve@gmail.com>
2022-10-06 13:07:59 +0300

    Revert "Add DateTime.pas"
    This reverts commit 8adf9a822fa281b86ddbad25b8ee020c731d174d.

--- DateTime.pas
+++
@@ -1,5 +0,0 @@
-﻿begin
-  var dt:datetime;
-  if (datetime.Now.Year mod 4 = 0) and (datetime.Now.Year mod 100 <> 0) or (datetime.Now.Year mod 400 = 0) then print('Год високосный')
-    else print('Год невисокосный');
-end.
