8adf9a822fa281b86ddbad25b8ee020c731d174d
Никита Коломыцев <shishki4l0ve@gmail.com>
2022-10-06 13:06:21 +0300

    Add DateTime.pas

---
+++ DateTime.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var dt:datetime;
+  if (datetime.Now.Year mod 4 = 0) and (datetime.Now.Year mod 100 <> 0) or (datetime.Now.Year mod 400 = 0) then print('Год високосный')
+    else print('Год невисокосный');
+end.
